ca69c42a9655a3537d658644d47430c61a4e9786
soundaryaBE <soundaryaraj62@gmail.com>
2024-07-17 14:10:31 +0530

    third local commit

--- src/test/java/DummyPackage/DummyClass.java
+++ src/test/java/DummyPackage/DummyClass.java
@@ -4,8 +4,9 @@ public class DummyClass {
 	public static void main(String[] args) {
 		System.out.println("First dummy");
 		System.out.println("Second dummy");
-		System.out.println("soundarya changes");
+		System.out.println("soundarya changes1");
 		System.out.println("third dummy");
 		System.out.println("fourth dummy");
+		System.out.println("soundarya changes2");
 	}
 }
