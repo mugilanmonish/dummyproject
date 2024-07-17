f29838c5671e27caa647b52245a4fae4db89e426
mugilanmonish <147095455+mugilanmonish@users.noreply.github.com>
2024-07-17 12:21:33 +0530

    Update DummyClass.java

--- src/test/java/DummyPackage/DummyClass.java
+++ src/test/java/DummyPackage/DummyClass.java
@@ -4,5 +4,6 @@ public class DummyClass {
 	public static void main(String[] args) {
 		System.out.println("First dummy");
 		System.out.println("Second dummy");
+		System.out.println("third dummy");
 	}
 }
