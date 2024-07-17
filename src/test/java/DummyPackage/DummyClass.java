cbb33361b3d79bff0468021f8ed6139f3e6effd3
mugilanmonish <147095455+mugilanmonish@users.noreply.github.com>
2024-07-17 12:31:48 +0530

    Update DummyClass.java

--- src/test/java/DummyPackage/DummyClass.java
+++ src/test/java/DummyPackage/DummyClass.java
@@ -4,10 +4,7 @@ public class DummyClass {
 	public static void main(String[] args) {
 		System.out.println("First dummy");
 		System.out.println("Second dummy");
-
 		System.out.println("soundarya changes");
-
 		System.out.println("third dummy");
-
 	}
 }
