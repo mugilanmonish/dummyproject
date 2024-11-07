baadc9b22d0680dcc9377775695ac64370f23ff2
mugilanmonish <147095455+mugilanmonish@users.noreply.github.com>
2024-11-07 23:02:39 +0530

    Update DummyClass.java

--- src/test/java/DummyPackage/DummyClass.java
+++ src/test/java/DummyPackage/DummyClass.java
@@ -2,17 +2,6 @@ package DummyPackage;
 
 public class DummyClass {
 	public static void main(String[] args) {
-		System.out.println("First dummy");
-		System.out.println("Second dummy");
-		System.out.println("soundarya changes");
-		System.out.println("third dummy");
-		System.out.println("fourth dummy");
-		System.out.println("fifth dummy");
-		System.out.println("sixth dummy");
-		System.out.println("seventh dummy");
-		System.out.println("eighth dummy");
-		System.out.println("ninth dummy");
-		System.out.println("tenth dummy");
-		System.out.println("eleventh dummy");
+		
 	}
 }
